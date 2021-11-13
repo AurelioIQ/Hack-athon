2503fb0ca73aad1f4ff68c8377dee4f36737b227
reinosoe <reinosoe@uninorte.edu.co>
2021-11-13 17:27:16 -0500

    adiccion de conexion base de datos

--- models.py
+++ models.py
@@ -1,17 +1,46 @@
-import sqlite3
-from sqlite3 import Error
-from flask import g
+from flask import Flask, render_template, flash, request, url_for, redirect, session
+from db import *
 
-def get_db():
-    try:
-        if 'db' not in g:
-            g.db=sqlite3.connect('db/orion.db')
-        return g.db
-    except Error:
-        print(Error)
+#obtener rol y creacion usuario
+"""def creacionusuario(): 
+    if "id_user" in session and session["rol"] == "superadministrador":
+        db = get_db()
+        roles = db.execute(
+            'SELECT id, rol FROM roles'
+        ).fetchall()
+        if request.method == 'POST':
+            # POST:
+            nombres= request.form['nombre']
+            apellidos= request.form['apellidos']
+            documentos = request.form['documentos']
+            identificacion = request.form['identificacion']
+            usuario = request.form['usuario']
+            contrasena = request.form['contrasena']
+            roles = request.form['roles']
+            if nombres == "" or apellidos == "" or documentos == "" or identificacion == "" or usuario == "" or contrasena == "" or roles == "":
+                error = 'Al menos un dato no fue ingresado'
+                flash( error )
+                return redirect(url_for("creacionusuario"))
+            else:
+                conn = get_db()        
+                cursorObj = conn.cursor()        
+                cursorObj.execute(
+                    "INSERT INTO usuario (nombres, apellidos, documento, identificacion, usuario, contrasena, roles) VALUES (?,?,?,?,?,?,?)",(nombres, apellidos, documentos, identificacion, usuario, generate_password_hash(contrasena), roles)
+                )
+                print("Aquí.")
+                conn.commit() 
+                conn.close()
+                return redirect(url_for("creacionusuario"))
+            
+        return render_template('creacionUsuario.html', roles=roles)
+    else:
+        session.clear()
+        return redirect(url_for("index"))"""
 
-def close_db():
-    db=g.pop('db', None)
-    if db is not None:
-        db.close()
-
+#buscar producto
+def buscarproductos(buscaproductos): 
+    db = get_db()
+    producto = db.execute(
+        'SELECT * FROM productos WHERE nombre_producto="{}" COLLATE NOCASE or codigo_producto="{}" COLLATE NOCASE'.format(buscaproductos,buscaproductos)
+    ).fetchall()
+    return(producto)
